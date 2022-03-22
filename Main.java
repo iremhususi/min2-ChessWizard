bce39dd2e2fdc5576f8a1268ed75a2659ac50890
ChessWizard <88746775+ChessWizard@users.noreply.github.com>
2022-03-22 16:20:20 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
+import java.lang.*;
+import java.util.Scanner;
 public class Main {
     public static void main(String args[]){
+        
+        System.out.println("Lutfen sirasiyla 2 sayi giriniz: ");
+        Scanner scanner = new Scanner(System.in);
+        int sayi1 = scanner.nextInt();
+        int sayi2 = scanner.nextInt();
+        
+        System.out.println(enKucukSayi(sayi1,sayi2));
+    }
     
+    public static int enKucukSayi(int a, int b){
+    
+        if(a<b){
+            return a;
+        }
+        
+        else if(b<a){
+            return b; 
+        }
+       return 0;
     }
 }
